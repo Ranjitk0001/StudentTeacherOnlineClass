d4eb80f2a754fa854753729f7242d78ea74d25e6
rk <ranjitkale0001@gmail.com>
2024-11-29 20:30:21 +0530

    Show Student list

--- StudentTeacherOnlineClass/src/main/java/com/AngularSpringBootMysql/demo/controller/StudentController.java
+++ StudentTeacherOnlineClass/src/main/java/com/AngularSpringBootMysql/demo/controller/StudentController.java
@@ -1,6 +1,13 @@
 package com.AngularSpringBootMysql.demo.controller;
 
+import java.util.List;
+
+import javax.management.AttributeNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +26,11 @@ public class StudentController {
 	public Student createStudent(@RequestBody Student student) {
 		return studentRepository.save(student);
 	}
+	
+	@GetMapping("/showlist")
+	  public List<Student> getAllPatients(){
+		  return studentRepository.findAll();
+	  }
+	
 
 }
