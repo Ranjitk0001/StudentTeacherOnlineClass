37326a574385f9da33ee860fbccfebe533eb574c
rk <ranjitkale0001@gmail.com>
2024-11-29 19:35:11 +0530

    Create spring boot project
    new-> spring starter project (Mavem, ProjectName)
    
    Adding dependencies :- lombok, web-spring,JpaData,MySql, Spring BootDevTools.

---
+++ StudentTeacherOnlineClass/src/main/java/com/AngularSpringBootMysql/demo/StudentTeacherOnlineClassApplication.java
@@ -0,0 +1,13 @@
+package com.AngularSpringBootMysql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTeacherOnlineClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTeacherOnlineClassApplication.class, args);
+	}
+
+}
