294a9ad20c5844ced97796d678723f129884080c
rk <ranjitkale0001@gmail.com>
2024-12-01 17:31:02 +0530

    @Repository

--- StudentTeacherOnlineClass/src/main/java/com/AngularSpringBootMysql/demo/repository/StudentRepository.java
+++ StudentTeacherOnlineClass/src/main/java/com/AngularSpringBootMysql/demo/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.AngularSpringBootMysql.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.AngularSpringBootMysql.demo.entity.Student;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
 
 }
